20ac862838ec52d17cacfa04bf8e281905fff96c
Farooqfar <info4895@gmail.com>
2025-08-08 19:46:03 +0500

    not

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,37 +1,26 @@
 import { writeFile } from "fs/promises";
-import Post from "@/lib/post";
 import { NextResponse } from "next/server";
-
-
+import Post from "@/lib/post";
 export async function POST(req) {
-    try {
-        const data = await req.formData();
-
-        const fullname = data.get("fullname");
-        const description = data.get("description");
-        const price = data.get("price");
-        const file = data.get("image");
-
-        const bytes = await file.arrayBuffer();
-        const buffer = Buffer.from(bytes);
-
-        const base64 = buffer.toString("base64");
-        const mimeType = file.type;
-        const base64Image = `data:${mimeType};base64,${base64}`;
-
-        const post = await Post.create({
-            fullname,
-            description,
-            price,
-            image: base64Image // store as base64
-        });
-
-        return NextResponse.json({ post });
-    } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
-    }
+    const data = await req.formData();
+    const fullname = data.get("fullname")
+    const description = data.get("description")
+    const price = data.get("price")
+    const file = data.get("image")
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+    const path = `./public/${file.name}`
+    await writeFile(path, buffer)
+    const post = await Post.create({
+        fullname,
+        description,
+        price,
+        image: file.name
+    })
+    return NextResponse.json({
+        post
+    })
 }
-
 
 export async function GET(req, res) {
     let allPost = await Post.find();
